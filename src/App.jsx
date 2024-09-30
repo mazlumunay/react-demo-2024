a4718016b550465e3aa2477f8c1e80306260fc58
qejmer <unaymazlum@gmail.com>
2024-09-30 19:36:58 -0400

    add react jobs demo

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  Route, 
+  createBrowserRouter, 
+  createRoutesFromElements,
+   RouterProvider
+ } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+import JobPages from './pages/JobsPages'
+import NotFoundPage from './pages/NotFoundPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout />}>
+    <Route index element={<HomePage />} />
+    <Route path='/jobs' element={<JobPages />} />
+    <Route path='*' element={<NotFoundPage />} />
+
+  </Route>  
+)
+);
+
+const App = () => {
+  return <RouterProvider router={router}/>;
+}
+
+export default App
