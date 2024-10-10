63c53da61bb13a2c666d5b1bebd53f4dea9c37c1
qejmer <unaymazlum@gmail.com>
2024-10-10 15:17:27 -0400

    Added single job page

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -35,7 +35,7 @@ const JobListing = ({jobs}) => {
                   {jobs.location}
                 </div>
                 <Link
-                  to={`/job/{jobs.id}`}
+                  to={`/jobs/${jobs.id}`}
                   className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
                  Read More
