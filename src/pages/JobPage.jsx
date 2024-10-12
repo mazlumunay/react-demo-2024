fc4e785dd78fc148cf9b08ea0adf8d57d0131ba4
qejmer <unaymazlum@gmail.com>
2024-10-12 00:34:08 -0400

    Add delete job functionality

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -31,16 +31,26 @@ const JobPage = () => {
 export default JobPage;
 */
 
-import { useParams, useLoaderData } from 'react-router-dom';
+import { useParams, useLoaderData, useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 
-const JobPage = () => {
+const JobPage = ({deleteJob}) => {
   const { id } = useParams();
   const job = useLoaderData();
+  const navigate = useNavigate();
 
-  
+
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm('Are you sure you want to delete that listing?')
+
+    if(!confirm) return;
+
+    deleteJob(jobId);
+
+    navigate('/jobs');
+  };
 
   return  (
     <>
@@ -123,8 +133,8 @@ const JobPage = () => {
                 >
                   Edit Job
               </Link>
-              <button
-                className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+              <button 
+                onClick={() => onDeleteClick(job.id)} className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job
               </button>
